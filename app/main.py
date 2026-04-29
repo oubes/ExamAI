cdfd88ea2c89fb7cd2ac946d3f6f98997bacd4de
Omar Gamal <157318256+oubes@users.noreply.github.com>
2026-04-29 11:45:40 +0300

    Chore/files (#34)
    * init test dir
    
    * rename test to tests dir
    
    * adding the script dir
    
    * adding the docker dir
    
    * adding the app
    
    * adding the agent_types, which identifies the AgentState data
    
    * added the basic orch for the system
    
    * adding fastapi to the main
    
    * adding the config settings for api
    
    * editing the graph name to orchestrator
    
    * adding dummy endpoint for testing
    
    * removing unwanted files
    
    * updating files struct
    
    * refactor: reorganize state graph structure and remove unused files
    
    * refactor: remove unused state graph components and clean up main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from app.core.state_graph.graph_manager import orchestrator
 from app.api.v1.endpoints.submission import submit
 
 app = FastAPI(
@@ -17,15 +16,6 @@ app.include_router(
 
 print("DATABASE_URL:", settings.DATABASE_URL)
 print("VERSION:", settings.version)
-
-
-## Testing
-result = orchestrator.invoke(
-    {
-        "msg": "Hello World" 
-    } # type: ignore
-)
-print(result)
 
 print("DATABASE_URL:", settings.DATABASE_URL)
 print("VERSION:", settings.version)
