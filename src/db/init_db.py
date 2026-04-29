472d61e9f51ac4216294e63fce79dce2563af3dc
oubes <omargamal.bs99@gmail.com>
2026-04-29 21:17:11 +0300

    feat: import identity model in database initialization

--- src/db/init_db.py
+++ src/db/init_db.py
@@ -1,6 +1,8 @@
 from db.base import Base
 from db.session import engine
 
+from db.models import identity
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
