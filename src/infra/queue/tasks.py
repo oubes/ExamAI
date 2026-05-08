a3c68c2b66b41e1ba42e4e88151544f4e683a487
oubes <omargamal.bs99@gmail.com>
2026-05-08 19:59:01 +0300

    refactor(queue): replace asyncio.run with async_to_sync for celery tasks
    - Remove email_tasks.py and create a new tasks.py file
    - Replace run_async function with async_to_sync from asgiref.sync
    - Update imports in identity_routes.py to reflect the new file name
    - Add asgiref package to requirements.txt

---
+++ src/infra/queue/tasks.py
@@ -0,0 +1,43 @@
+# ---- Imports ---- #
+from asgiref.sync import async_to_sync
+
+from src.infra.queue.celery_app import celery_app
+from src.infra.email.service import EmailService
+
+
+# ---- Services ---- #
+email_service = EmailService()
+
+
+# --------------- Mail Tasks --------------- #
+# ---- Welcome Email Task ---- #
+@celery_app.task
+def send_welcome_email(to: str, context: dict):
+
+    return async_to_sync(email_service.send_from_template)(
+        to=to,
+        template_name="welcome.yml",
+        context=context,
+    )
+
+
+# ---- Verify Email Task ---- #
+@celery_app.task
+def send_verify_email(to: str, context: dict):
+
+    return async_to_sync(email_service.send_from_template)(
+        to=to,
+        template_name="verify_email.yml",
+        context=context,
+    )
+
+
+# ---- Reset Password Email Task ---- #
+@celery_app.task
+def send_reset_password_email(to: str, context: dict):
+
+    return async_to_sync(email_service.send_from_template)(
+        to=to,
+        template_name="reset_password.yml",
+        context=context,
+    )
